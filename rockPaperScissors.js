091bb290a9d6587a8c46e93b638181e54ca854c8
Aye Chan <ayechan.sant@gmail.com>
2022-12-28 18:30:17 -0500

    play the game in js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let num = Math.floor(Math.random * 2)
+    let num = Math.floor(Math.random() * 2)
     if (num === 0) {
         return 'rock'
     } else if (num === 1) {
@@ -11,16 +11,30 @@ function getComputerChoice() {
     }
 };
 
-function getUserChoice()
-
-
-
-
-
-
-
-
-
-
 let computerChoice = getComputerChoice();
+//let humanChoice = prompt('human choose rock paper or scissors: ')
 
+function playGame(computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        return 'tie'
+    }
+    if (computerChoice === 'rock') {
+        if (humanChoice === 'paper') {
+            return 'human def wins'
+        } else {
+            return 'computer wins absolutely'
+        }
+    } else if (computerChoice === 'paper') {
+        if (humanChoice === 'scissors') {
+            return 'human wins fosho'
+        } else {
+            return 'computer annhilates'
+        }
+    } else if (computerChoice === 'scissors') {
+        if (humanChoice === 'rock') {
+            return 'human emerges victorious'
+        } else {
+            return 'computer stores victory to memory'
+        }
+    }
+}
