b690df95f311c2bb0220f594b38fd403ae075632
Aye Chan <ayechan.sant@gmail.com>
2023-04-09 15:08:22 -0700

    1000 round plays

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,115 +1,89 @@
-"use strict";
+'use strict';
 
 const rockButton = document.getElementById('rock-button');
 const paperButton = document.getElementById('paper-button');
 const scissorsButton = document.getElementById('scissors-button');
-const resultDisplay = document.getElementById('result');
-const scoreDisplay = document.getElementById('score');
+const humanDisplay = document.querySelector('.human-score');
+const computerDisplay = document.querySelector('.computer-score');
+const thousandRoundButton = document.getElementById('thousand-button');
+let humanChoice = '';
 
-// function playSound() {
-//     const audio = new Audio('sounds/gun.mp3');           //in progress to add sound
-//     audio.play(); 
-// }
+rockButton.addEventListener('click', () => {
+  humanChoice = 'rock';
+});
 
+paperButton.addEventListener('click', () => {
+  humanChoice = 'paper';
+});
 
-function getComputerChoice() {                      //returns a string (rock, paper, or scissors)
-    let num = Math.floor(Math.random() * 3)
-    if (num === 0) {
-        return 'rock';
-    } else if (num === 1) {
-        return 'paper';
-    } else if (num === 2) {
-        return 'scissors';
-    } else {
-        return 'ERROR: computer choice';
-    }
-};
+scissorsButton.addEventListener('click', () => {
+  humanChoice = 'scissors';
+});
 
-rockButton.addEventListener("click", () => {
-    playGame('rock');
-})
+function playSound() {
+  const audio = new Audio('sounds/gun.mp3'); //in progress to add sound
+  audio.play();
+}
 
-paperButton.addEventListener("click", () => {
-    playGame('paper');
-})
+function getComputerChoice() {
+  //returns a string (rock, paper, or scissors)
+  let num = Math.floor(Math.random() * 3);
+  if (num === 0) {
+    return 'rock';
+  } else if (num === 1) {
+    return 'paper';
+  } else if (num === 2) {
+    return 'scissors';
+  } else {
+    return 'ERROR: computer choice';
+  }
+}
 
-scissorsButton.addEventListener("click", () => {
-    playGame('scissors');
-})
-
-// thousandRoundButton.addEventListener("click", () => {            //in progress to 1,000 rounds
-//     playThousand('rock')
-// })
-    
 let computerWin = 0;
 let humanWin = 0;
+let ties = 0;
 
-function playGame(humanChoice) {
-
-    function playRound(humanChoice) {
-        let computerChoice = getComputerChoice();
-        if (computerChoice === humanChoice) {
-            return 'tie';
-        }
-        if (computerChoice === 'rock') {  
-            return humanChoice === 'paper' 
-            ? 'human win'
-            : 'computer win'
-        }
-        if (computerChoice === 'paper') {
-            return humanChoice === 'scissors' 
-            ? 'human win'
-            : 'computer win'
-        }
-        if (computerChoice === 'scissors') {
-            return humanChoice === 'rock' 
-            ? 'human win'
-            : 'computer win'
-        }
-    }
-
-    let result = playRound();
-
-    if (result === 'tie') {
-        resultDisplay.innerHTML = 'tie game';
-        scoreDisplay.innerHTML = 'H: ' + humanWin + ' ' + 'C: ' + computerWin;
-    } else if (result === 'human win') {
-        humanWin++;
-        resultDisplay.innerHTML = 'human won the round';
-        scoreDisplay.innerHTML = 'H: ' + humanWin + ' ' + 'C: ' + computerWin;
-        if (humanWin >= 3) {
-            resultDisplay.innerHTML = 'HUMAN WON GAME';
-        } else if (computerWin >= 3) {
-            resultDisplay.innerHTML = 'COMPUTER WON GAME';
-        }
-    } else if (result === 'computer win') {
-        computerWin++;
-        resultDisplay.innerHTML = 'computer won the round';
-        scoreDisplay.innerHTML = 'H: ' + humanWin + ' ' + 'C: ' + computerWin;
-        if (humanWin >= 3) {
-            resultDisplay.innerHTML = 'HUMAN WON GAME';
-        } else if (computerWin >= 3) {
-            resultDisplay.innerHTML = 'COMPUTER WON GAME';
-        }
+function playRound() {
+  let humanChoice = getComputerChoice();
+  let computerChoice = getComputerChoice();
+  if (computerChoice === humanChoice) {
+    ties += 1;
+  } else if (computerChoice === 'rock') {
+    if (humanChoice === 'paper') {
+      humanWin = humanWin += 1;
     } else {
-        return 'ERROR: play round';
+      computerWin = computerWin += 1;
     }
-};
+  } else if (computerChoice === 'paper') {
+    if (humanChoice === 'scissors') {
+      humanWin = humanWin += 1;
+    } else {
+      computerWin = computerWin += 1;
+    }
+  } else if (computerChoice === 'scissors') {
+    if (humanChoice === 'rock') {
+      humanWin = humanWin += 1;
+    } else {
+      computerWin = computerWin += 1;
+    }
+  }
+}
 
-// function playThousand(rock) {
-//     for (i = 0; i <= 1000; i++) {
-//         playRound(rock);
-//         if (result === 'tie') {
-//             resultDisplay.innerHTML = 'tie game';
-//             scoreDisplay.innerHTML = 'H: ' + humanWin + ' ' + 'C: ' + computerWin;        //in progress to add sound
-//         } else if (result === 'human win') {     
-//             humanWin++;
-//             resultDisplay.innerHTML = 'human won the round';
-//             scoreDisplay.innerHTML = 'H: ' + humanWin + ' ' + 'C: ' + computerWin;
-//         } else if (result === 'computer win') {
-//             computerWin++;
-//             resultDisplay.innerHTML = 'computer won the round';
-//             scoreDisplay.innerHTML = 'H: ' + humanWin + ' ' + 'C: ' + computerWin;
-//         }
-//     }   
-// }
+function playGame() {
+  for (let i = 0; i < 10; i++) {
+    setTimeout(() => {
+      playRound();
+      playSound();
+      console.log('round: ' + i);
+      console.log(ties);
+      console.log(humanWin);
+      console.log(computerWin);
+      humanDisplay.innerHTML = humanWin;
+      computerDisplay.innerHTML = computerWin;
+    }, 1000);
+  }
+}
+
+thousandRoundButton.addEventListener('click', () => {
+  playGame();
+});
